148c33ef097c40106052e29fd44167b269b3f70b
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2019-06-10 19:16:53 +0200

    Get CDI Decorator example to work the right way
    * Add priority annotation to decorator.

--- comacmeconsumer/src/main/java/com/acme/service/consumer/ServiceDecorator.java
+++ comacmeconsumer/src/main/java/com/acme/service/consumer/ServiceDecorator.java
@@ -1,11 +1,14 @@
 package com.acme.service.consumer;
 
 import com.acme.service.api.Service;
+import javax.annotation.Priority;
 import javax.decorator.Decorator;
 import javax.decorator.Delegate;
 import javax.inject.Inject;
+import javax.interceptor.Interceptor;
 
 @Decorator
+@Priority(Interceptor.Priority.APPLICATION)
 public class ServiceDecorator implements Service {
 
     @Delegate
